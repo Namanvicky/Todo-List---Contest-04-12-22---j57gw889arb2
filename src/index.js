afb25b7f2a4d7aed6ffed0d23342950a73e044a3
Namanvicky <116458366+Namanvicky@users.noreply.github.com>
2022-12-04 14:57:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+@@ -1,5 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import React, { useState } from "react";
 
+ReactDOM.render(<App />, document.getElementById("root"));
