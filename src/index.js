6e168cf888c48b65c31ec85ef5b5728fec9b860f
Namanvicky <116458366+Namanvicky@users.noreply.github.com>
2022-12-04 14:51:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React, { useState } from "react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default function Task(props) {
+  const { task, saveChangedText, handleDelete } = props;
+
+  const [showEdit, setShowEdit] = useState(false);
+  const [editText, setEditText] = useState("");
+
+  const handleEdit = (id) => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleTextChange = (event) => {
+    setEditText(event.target.value);
+  };
+
+  const saveNewText = (id) => {
+    saveChangedText(id, editText);
+    setShowEdit(false);
+    setEditText("");
+  };
+
+  return (
+    <div>
+      <li className="list">{task.text}</li>
+      <button className="edit" onClick={() => handleEdit(task.id)}>
+        Edit
+      </button>
+      <button className="delete" onClick={() => handleDelete(task.id)}>
+        Delete
+      </button>
+      {showEdit ? (
+        <div>
+          <input
+            type="texarea"
+            className="editTask"
+            onChange={handleTextChange}
+          ></input>
+          <button
+            className="saveTask"
+            disabled={!editText}
+            onClick={() => saveNewText(task.id)}
+          >
+            Save
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
